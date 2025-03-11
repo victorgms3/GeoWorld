6e3e9ddefcea80e443849065ec13f1f1bc914d3a
Victor <victorgms0307@gmail.com>
2025-03-11 15:45:54 +0100

    geoworld

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Fragment Page Footer
+ *
+ * PHP version 7
+ *
+ * @category  Page_Footer
+ * @package   Application
+ * @author    SIO-SLAM <sio@ldv-melun.org>
+ * @copyright 2023-2024 SIO-SLAM
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link      https://github.com/sio-melun/geoworld
+ */
+
+?>
+<hr />
+<footer class="footer mt-auto py-3">
+  <div class="container">
+    <span class="text-muted">&copy;2023-2024 SIO SLAM MyWebApp</span>
+  </div>
+</footer>
+
+  </body>
+</html>
